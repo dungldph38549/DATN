f13724e26db88a46307eafe357998cf31d407afb
daotuan05 <tuandnph50338@gmail.com>
2025-05-31 21:34:40 +0700

    them base

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import{ Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import { routers } from './router/index.js'
-import HomePages from "./pages/HomePages/HomePages.tsx";
 
 
 function App() {
@@ -13,11 +12,9 @@ function App() {
             {routers.map((route) => {
               const Page = route.page;
               return (
-                
               <Route path={route.path} element={<Page/>}/>
               )
             })}
-              <Route path='/' element={<HomePages/>}/>
           </Routes>
         </Router>
     </div>
